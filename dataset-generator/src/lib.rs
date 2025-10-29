7696398686b9e2adf545f4eb98da04b514423502
Forjeon <forjeon22i7@gmail.com>
2025-10-28 23:02:18 -0600

    Left TODO notes for next development steps; created ConwayBoard type alias; refined run() signature and prototype board generation code

--- dataset-generator/src/lib.rs
+++ dataset-generator/src/lib.rs
@@ -2,9 +2,17 @@ use rand::distr::{Bernoulli, Distribution};
 
 const BOARD_WIDTH: usize = 8;
 
-pub fn run(num_instances: u32, max_timesteps: u32) -> [bool; BOARD_WIDTH * BOARD_WIDTH] {
-	//let mut board: [bool; BOARD_WIDTH * BOARD_WIDTH] = [false; BOARD_WIDTH * BOARD_WIDTH];
-	//let d = Bernoulli::new(0.3).unwrap();
-	//let v = d.sample(&mut rand::rng());
+type ConwayBoard = [bool; BOARD_WIDTH * BOARD_WIDTH];
+
+// Returns a vector of instances, each being a tuple of initial board, timestep, and target board
+pub fn run(num_instances: u32, max_timesteps: u32, sparsity: f32) -> Vec<(ConwayBoard, u32, ConwayBoard)> {
+	// TODO: separate into functions for generating a board and advancing a board
+	let bernoulli_distr = Bernoulli::new(sparsity.into()).unwrap();
+	let mut rng = rand::rng();
+
+	let mut board: [bool; BOARD_WIDTH * BOARD_WIDTH] = [false; BOARD_WIDTH * BOARD_WIDTH];
+	for cell in &mut board {
+		*cell = bernoulli_distr.sample(&mut rng);
+	}
 	todo!();
 }
--- dataset-generator/src/main.rs
+++ dataset-generator/src/main.rs
@@ -5,5 +5,5 @@ use dataset_generator::run;
 fn main() {
 	let args: Vec<String> = env::args().collect();
     println!("Hello, world!");
-	run();
+	//run();
 }
