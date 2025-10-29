d3a9de19f3189911b330220a537b98e353a9bf48
Forjeon <forjeon22i7@gmail.com>
2025-10-28 22:53:30 -0600

    Started on dataset generator

---
+++ dataset-generator/src/lib.rs
@@ -0,0 +1,10 @@
+use rand::distr::{Bernoulli, Distribution};
+
+const BOARD_WIDTH: usize = 8;
+
+pub fn run(num_instances: u32, max_timesteps: u32) -> [bool; BOARD_WIDTH * BOARD_WIDTH] {
+	//let mut board: [bool; BOARD_WIDTH * BOARD_WIDTH] = [false; BOARD_WIDTH * BOARD_WIDTH];
+	//let d = Bernoulli::new(0.3).unwrap();
+	//let v = d.sample(&mut rand::rng());
+	todo!();
+}
---
+++ dataset-generator/src/main.rs
@@ -0,0 +1,9 @@
+use std::env;
+
+use dataset_generator::run;
+
+fn main() {
+	let args: Vec<String> = env::args().collect();
+    println!("Hello, world!");
+	run();
+}
