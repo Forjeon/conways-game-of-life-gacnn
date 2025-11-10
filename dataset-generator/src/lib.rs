f450eafe2538a4ae2ca1789ee44a053d475d884d
Forjeon <forjeon22i7@gmail.com>
2025-11-10 13:03:25 -0700

    Finished red-green TDD of ConwayGame.get_cell()

--- dataset-generator/src/lib.rs
+++ dataset-generator/src/lib.rs
@@ -24,11 +24,13 @@ impl ConwayGame {
 		ConwayGame{ generation }
 	}
 
-	fn get_cell(&self, x: usize, y: usize) -> bool {
-		todo!()
+	fn get_cell(&self, x: isize, y: isize) -> bool {
+		let x = 0usize.wrapping_add_signed(x);
+		let y = 0usize.wrapping_add_signed(y);
+		self.generation[(x % BOARD_WIDTH) + BOARD_WIDTH * (y % BOARD_WIDTH)]
 	}
 
-	fn get_live_neighbours(&self, x: usize, y: usize) -> u8 {
+	fn get_live_neighbors(&self, x: usize, y: usize) -> u8 {
 		todo!()
 	}
 
@@ -43,8 +45,6 @@ impl ConwayGame {
 
 // Returns a vector of instances, each being a tuple of initial board, timestep, and target board
 pub fn run(num_instances: u32, max_timesteps: u32, sparsity: f32) -> Vec<(ConwayBoard, u32, ConwayBoard)> {
-	let mut board = ConwayGame::new(sparsity);
-	board.tick_by(5);
 	todo!()
 }
 
@@ -63,14 +63,122 @@ fn test_get_cell() {
 	] };
 	for x in 0..BOARD_WIDTH {
 		for y in 0..BOARD_WIDTH {
-			assert!(!game0.get_cell(x, y));
+			assert!(!game0.get_cell(x.try_into().unwrap(), y.try_into().unwrap()));
+		}
+	}
+
+	let mut game1 = ConwayGame { generation: [
+		true, false, true, false, true, false, true, false,
+		true, false, true, false, true, false, true, false,
+		true, false, true, false, true, false, true, false,
+		true, false, true, false, true, false, true, false,
+		true, false, true, false, true, false, true, false,
+		true, false, true, false, true, false, true, false,
+		true, false, true, false, true, false, true, false,
+		true, false, true, false, true, false, true, false,
+	] };
+	for y in 0..BOARD_WIDTH {
+		for x in 0..BOARD_WIDTH {
+			match x % 2 {
+				0 => assert!(game1.get_cell(x.try_into().unwrap(), y.try_into().unwrap())),
+				_ => assert!(!game1.get_cell(x.try_into().unwrap(), y.try_into().unwrap())),
+			}
+		}
+	}
+
+	let mut game2 = ConwayGame { generation: [
+		true, true, true, true, true, true, true, true,
+		true, true, true, true, true, true, true, true,
+		true, true, true, true, true, true, true, true,
+		true, true, true, true, true, true, true, true,
+		true, true, true, true, true, true, true, true,
+		true, true, true, true, true, true, true, true,
+		true, true, true, true, true, true, true, true,
+		true, true, true, true, true, true, true, true,
+	] };
+	for x in 0..BOARD_WIDTH {
+		for y in 0..BOARD_WIDTH {
+			assert!(game2.get_cell(x.try_into().unwrap(), y.try_into().unwrap()));
 		}
 	}
 }
 
+#[test]
+fn test_get_cell_toroidal() {
+	let mut game0 = ConwayGame { generation: [
+		true, false, true, false, true, false, true, false,
+		true, false, true, false, true, false, true, false,
+		true, false, true, false, true, false, true, false,
+		true, false, true, false, true, false, true, false,
+		true, false, true, false, true, false, true, false,
+		true, false, true, false, true, false, true, false,
+		true, false, true, false, true, false, true, false,
+		true, false, true, false, true, false, true, false,
+	] };
+	// Test (0, 0) toroidal neighbors
+	assert!(!game0.get_cell(-1, -1));
+	assert!(game0.get_cell(0, -1));
+	assert!(!game0.get_cell(1, -1));
+	assert!(!game0.get_cell(-1, 0));
+	assert!(game0.get_cell(0, 0));
+	assert!(!game0.get_cell(1, 0));
+	assert!(!game0.get_cell(-1, 1));
+	assert!(game0.get_cell(0, 1));
+	assert!(!game0.get_cell(1, 1));
+	// Test (7, 7) toroidal neighbors
+	assert!(game0.get_cell(6, 6));
+	assert!(!game0.get_cell(7, 6));
+	assert!(game0.get_cell(8, 6));
+	assert!(game0.get_cell(6, 7));
+	assert!(!game0.get_cell(7, 7));
+	assert!(game0.get_cell(8, 7));
+	assert!(game0.get_cell(6, 8));
+	assert!(!game0.get_cell(7, 8));
+	assert!(game0.get_cell(8, 8));
+	// Test extreme cases
+	assert!(!game0.get_cell(-1384061901, -1384061901));	// (3, 3)
+	assert!(game0.get_cell(-4, -4));					// (4, 4)
+	assert!(game0.get_cell(1187311368, 1187311368));	// (0, 0)
+	assert!(!game0.get_cell(23, 13));					// (7, 5)
+
+	let mut game1 = ConwayGame { generation: [
+		false, true, false, true, false, true, false, true,
+		false, true, false, true, false, true, false, true,
+		false, true, false, true, false, true, false, true,
+		false, true, false, true, false, true, false, true,
+		false, true, false, true, false, true, false, true,
+		false, true, false, true, false, true, false, true,
+		false, true, false, true, false, true, false, true,
+		false, true, false, true, false, true, false, true,
+	] };
+	// Test (0, 0) toroidal neighbors
+	assert!(game1.get_cell(-1, -1));
+	assert!(!game1.get_cell(0, -1));
+	assert!(game1.get_cell(1, -1));
+	assert!(game1.get_cell(-1, 0));
+	assert!(!game1.get_cell(0, 0));
+	assert!(game1.get_cell(1, 0));
+	assert!(game1.get_cell(-1, 1));
+	assert!(!game1.get_cell(0, 1));
+	assert!(game1.get_cell(1, 1));
+	// Test (7, 7) toroidal neighbors
+	assert!(!game1.get_cell(6, 6));
+	assert!(game1.get_cell(7, 6));
+	assert!(!game1.get_cell(8, 6));
+	assert!(!game1.get_cell(6, 7));
+	assert!(game1.get_cell(7, 7));
+	assert!(!game1.get_cell(8, 7));
+	assert!(!game1.get_cell(6, 8));
+	assert!(game1.get_cell(7, 8));
+	assert!(!game1.get_cell(8, 8));
+	// Test extreme cases
+	assert!(game1.get_cell(241777899, 14));		// (3, 4)
+	assert!(!game1.get_cell(-126, 241777899));	// (6, 3)
+}
+
 /*
 #[test]
-fn test_get_live_neighbours() {
+fn test_get_live_neighbors() {
 	todo!()
 }
 
